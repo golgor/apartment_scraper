4d9bb8f52bdc05f7294e24e115bae6e94ddeb5f7
Robert <golgafrincham@gmail.com>
2023-02-19 22:08:14 +0100

    add load_raw

--- apartment_scraper/data_loader.py
+++ apartment_scraper/data_loader.py
@@ -6,11 +6,11 @@ from apartment_scraper import Apartment, Site, pkg_path
 
 class DataLoader:
     def __init__(self, site: Site):
-        self.clean_path = pkg_path.joinpath(site.value, "clean_data")
-        self.raw_path = pkg_path.joinpath(site.value, "raw_data")
+        self._clean_path = pkg_path.joinpath(site.value, "clean_data")
+        self._raw_path = pkg_path.joinpath(site.value, "raw_data")
 
     def load_clean_data(self, filename: str) -> list[Apartment]:
-        filepath = self.clean_path.joinpath(filename)
+        filepath = self._clean_path.joinpath(filename)
         with open(filepath, "r") as f:
             data: dict[str, Any] = json.load(f)
 
@@ -18,3 +18,8 @@ class DataLoader:
             Apartment(id=id, **apartment_data)
             for id, apartment_data in data.items()
         ]
+
+    def load_raw_data(self, filename: str) -> list[dict[str, Any]]:
+        filepath = self._raw_path.joinpath(filename)
+        with open(filepath, "r") as f:
+            return json.load(f)
