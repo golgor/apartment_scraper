b3050dcce8de92da2f5076887a96b171fe7d6270
Robert <golgafrincham@gmail.com>
2023-02-21 21:24:41 +0100

    implement refactored willhaben package

--- apartment_scraper/__main__.py
+++ apartment_scraper/__main__.py
@@ -1,25 +1,33 @@
 import json
 from datetime import datetime
-from time import sleep
 
 import willhaben
 
-from apartment_scraper import pkg_path
+from apartment_scraper import Site, pkg_path
+from apartment_scraper.data_exporter import DataExporter
+from apartment_scraper.data_loader import DataLoader
 
 
-def main():
-    pass
+def get_willhaben_raw_data(filename: str):
+    wh = willhaben.WohnungenWien()
+    test = willhaben.get_data(wh)
 
-
-if __name__ == "__main__":
-    wh = willhaben.Requester()
-    today = datetime.now().strftime("%Y-%m-%d")
-    test = []
-    for data in wh:
-        sleep(1)
-        test.extend(data)
-
-    filepath = pkg_path.joinpath("data", f"willhaben_{today}.json")
+    filepath = pkg_path.joinpath("willhaben", "raw_data", filename)
     with open(filepath, "w") as f:
         f.write(json.dumps(test, indent=2))
         print(f"Successfully saved {len(test)}")
+
+
+def parse_willhaben_raw_data(filename: str):
+    dl = DataLoader(site=Site.WILLHABEN)
+    de = DataExporter(site=Site.WILLHABEN)
+    raw_data = dl.load_raw_data(filename)
+    apartments = willhaben.parse_willhaben_response(raw_data)
+    de.export_json(filename, apartments)
+
+
+if __name__ == "__main__":
+    today = datetime.now().strftime("%Y-%m-%d")
+    filename = f"willhaben_{today}.json"
+    # get_willhaben_raw_data(filename)
+    parse_willhaben_raw_data(filename)
