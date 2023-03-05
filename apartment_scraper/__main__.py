389b1627460f21d519940d6e4e12ff43bd941950
Robert <golgafrincham@gmail.com>
2023-03-05 22:22:47 +0100

    removed obsolete code

--- apartment_scraper/__main__.py
+++ apartment_scraper/__main__.py
@@ -1,66 +1,10 @@
-import json
-from datetime import datetime
-
-from apartment_scraper import Site, immowelt, pkg_path, willhaben
-from apartment_scraper.data_loader import DataLoader
-from apartment_scraper.models import Apartment, Model
-
-
-def get_willhaben_raw_data(obj: willhaben.Wohnungen | willhaben.Haus):
-    raw_data = willhaben.get_data(obj)
-
-    today = datetime.now().strftime("%Y-%m-%d")
-    filename = f"{obj.name}_{today}"
-
-    filepath = pkg_path.joinpath("willhaben", "raw_data", f"{filename}.json")
-    with open(filepath, "w") as f:
-        f.write(json.dumps(raw_data, indent=2))
-        print(f"Successfully saved {len(raw_data)}")
-
-
-def get_immowelt_raw_data(filename: str):
-    access_token = immowelt.get_immowelt_token()
-    request = immowelt.WohnungenWien(access_token)
-
-    test = immowelt.get_data(request)
-
-    filepath = pkg_path.joinpath("immowelt", "raw_data", f"{filename}.json")
-    with open(filepath, "w") as f:
-        f.write(json.dumps(test, indent=2))
-        print(f"Successfully saved {len(test)}")
-
-
-def parse_raw_data(filename: str, site: Site) -> list[Apartment]:
-    dl = DataLoader(site=site)
-
-    raw_data = dl.load_raw_data(filename)
-
-    if site == Site.IMMOWELT:
-        apartments = immowelt.parse_immowelt_response(raw_data)
-    elif site == Site.WILLHABEN:
-        apartments = willhaben.parse_willhaben_response(raw_data)
-    else:
-        raise ValueError("No valid site selected!")
-    return apartments
-
+from apartment_scraper import pkg_path, willhaben
+from apartment_scraper.models import Model
 
 if __name__ == "__main__":
     model = Model(path=pkg_path.joinpath("test.db"))
-    # areas = [
-    #     willhaben.AreaId.WIEN.ALL,
-    # ]
-
-    # for area in areas:
-    #     wh = willhaben.Wohnungen(area_id=area)
-    #     get_willhaben_raw_data(wh)
-
-    raw_data_path = pkg_path.joinpath("willhaben", "raw_data")
-    paths = raw_data_path.glob("wohnungen_wien_all*.json")
-    apartments: list[Apartment] = []
-    for path in paths:
-        parsed_path = str(path).split(".")[0]
-        apartments.extend(parse_raw_data(parsed_path, Site.WILLHABEN))
-
+    area = willhaben.AreaId.WIEN.ALL
+    wh = willhaben.Wohnungen(area_id=area)
+    raw_data = willhaben.get_data(wh)
+    apartments = willhaben.parse_willhaben_response(raw_data)
     model.add_apartments(apartments)
-    # print(len(apartments))
-    # de.export_csv("Wohnungen", apartments)
