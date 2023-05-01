799fe2c83f4a602e5ffa886bc1cd44c6598bb142
Robert <golgafrincham@gmail.com>
2023-05-01 09:16:29 +0200

    add more data to Schemas

--- apartment_scraper/api/schemas.py
+++ apartment_scraper/api/schemas.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -5,6 +7,16 @@ class ApartmentRentSchema(BaseModel):
     apartment_id: int
     area: int
     rent: int
+    url: str
+    rooms: float
+    floor: int
+    address: str
+    post_code: str
+    coordinates: Optional[str]
+    free_area_type: Optional[str]
+    free_area: Optional[int]
+    image_urls: Optional[list[str]]
+    apartment_type: str
 
     class Config:
         orm_mode = True
@@ -14,6 +26,16 @@ class ApartmentBuySchema(BaseModel):
     apartment_id: int
     area: int
     price: int
+    url: str
+    rooms: float
+    floor: int
+    address: str
+    post_code: str
+    coordinates: Optional[str]
+    free_area_type: Optional[str]
+    free_area: Optional[int]
+    image_urls: Optional[list[str]]
+    apartment_type: str
 
     class Config:
         orm_mode = True
